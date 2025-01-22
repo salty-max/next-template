3b574efab98463da14d19743acc11584b82e7e55
salty-max <max@jellycat.fr>
2025-01-22 10:46:00 +0100

    :tada: initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions & import('@ianvs/prettier-plugin-sort-imports').PluginConfig} */
+export default {
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "@ianvs/prettier-plugin-sort-imports",
+  ],
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
+  printWidth: 80,
+  endOfLine: "auto",
+  arrowParens: "always",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^@/env(.*)$",
+    "^@/types/(.*)$",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/styles/(.*)$",
+    "^@/app/(.*)$",
+    "",
+    "^[./]",
+  ],
+};
